7e95a8bdc1fc8bef515346c996fa67207fc4fa72
Tia Saxena <tia.saxena041202@gmail.com>
2021-10-10 17:45:01 +0530

    Update postorder in n-ary tree.cpp

--- C++/Errors/postorder in n-ary tree.cpp
+++ C++/Errors/postorder in n-ary tree.cpp
@@ -39,11 +39,11 @@ void postorder(TreeNode<int>*root){
     if(root==NULL){
         return ;
     }
-    cout<<root->data<<" ";
+    
     for(int i=0;i<root->children.size();i++){
         postorder(root->children[i]);
-    
     }
+    cout<<root->data<<" ";
 }
 
 int main(){
